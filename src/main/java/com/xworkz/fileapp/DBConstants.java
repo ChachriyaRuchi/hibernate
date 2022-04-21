fb799b69d1055977762df6043cbc4caf6d91bf90
ChachriyaRuchi <ruchixworkz@gmail.com>
2022-04-21 11:08:54 +0530

    com

---
+++ src/main/java/com/xworkz/fileapp/DBConstants.java
@@ -0,0 +1,10 @@
+package com.xworkz.fileapp;
+
+public interface DBConstants {
+
+	String JDBC_URL="jdbc:mysql://localhost:3306/newdatabase";
+	String USERNAME="root";
+	String PASSWORD="Ruchi@123";
+
+
+}
---
+++ src/main/java/com/xworkz/fileapp/Tester.java
@@ -0,0 +1,11 @@
+package com.xworkz.fileapp;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application ap=new Application();
+		ap.readData();
+	}
+
+}
